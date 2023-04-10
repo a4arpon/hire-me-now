4b1502957b28728e8a64ecf95297eca6b1ea547e
Mr. Xhr <a4arpon@gmail.com>
2023-04-10 20:41:19 +0600

    Saved Job Functionality Added

--- src/utilities/Utility.js
+++ src/utilities/Utility.js
@@ -16,14 +16,12 @@ const jobApply = (id) => {
   }
 };
 const appliedJobsLoader = async () => {
-  const loadedJobs = await fetch("/data.json")
-    .then((response) => response.json())
-    .catch((error) => console.error(error));
-  const savedObjects = JSON.parse(localStorage.getItem("appliedJobs")) || [];
-  const appliedJobs = loadedJobs.filter((apiObj) =>
-    savedObjects.find((savedObj) => savedObj.id === apiObj.id)
+  const loadedJobs = await fetch("/data.json").then((res) => res.json());
+  const savedAppliedJobs =
+    JSON.parse(localStorage.getItem("appliedJobs")) || [];
+  const appliedJobs = loadedJobs.filter((job) =>
+    savedAppliedJobs.find((savedAppliedJobs) => savedAppliedJobs === job.id)
   );
-  console.log("Common objects:", appliedJobs);
   return appliedJobs;
 };
 export { jobLoader, jobApply, appliedJobsLoader };
