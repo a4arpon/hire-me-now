92bbe6139410de2d90f133d2d7ee2691599a8628
Mr. Xhr <a4arpon@gmail.com>
2023-04-12 02:01:22 +0600

    Error Page Added

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <div className="bg-neutral-400 h-screen flex justify-center items-center">
+    <div className="bg-neutral-300 h-screen flex justify-center items-center text-gray-700">
       <div className="text-center">
-        <h1 className="text-white text-4xl font-extrabold">
+        <h1 className="text-4xl font-extrabold mb-16">
           Ops! Error 404. Page not found.
         </h1>
+        <Link
+          to="/"
+          className="text-2xl font-extrabold rounded-xl bg-red-400 p-5"
+        >
+          Back To Home
+        </Link>
       </div>
     </div>
   );
