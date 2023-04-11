db8a280d541ca09667bf56a84226c415aebc6a8b
Mr. Xhr <a4arpon@gmail.com>
2023-04-11 22:46:56 +0600

    Applied job filter added

--- src/pages/AppliedJobs/AppliedJobs.jsx
+++ src/pages/AppliedJobs/AppliedJobs.jsx
@@ -1,15 +1,42 @@
 import React from "react";
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import Banner from "./BannerAppliedJobs";
 import JobItem from "./JobItem";
 
 const AppliedJobs = () => {
   const unFilteredData = useLoaderData();
-  if (unFilteredData.length === 0) {
+  const [appliedJobData, setAppliedJobData] = useState(unFilteredData);
+  const filterByWhat = (value) => {
+    if (value === "on-site") {
+      const filteredData = unFilteredData.filter(
+        (jobsData) => jobsData.remote_or_onsite === "Onsite"
+      );
+      setAppliedJobData(filteredData);
+    } else if (value === "remote") {
+      const filteredData = unFilteredData.filter(
+        (jobsData) => jobsData.remote_or_onsite === "Remote"
+      );
+      setAppliedJobData(filteredData);
+    } else {
+      setAppliedJobData(unFilteredData);
+    }
+  };
+  if (appliedJobData.length === 0) {
     return (
       <div>
         <Banner />
         <div className="appliedJobContainer">
+          <div className="flex justify-end items-center mt-10">
+            <select
+              className="p-4 rounded-lg bg-gray-200 text-xl font-extrabold"
+              onChange={(btn) => filterByWhat(btn.target.value)}
+            >
+              <option>Filter By</option>
+              <option value="remote">Remote</option>
+              <option value="on-site">On-site </option>
+            </select>
+          </div>
           <div className="text-center my-20">
             <h1 className="text-4xl font-extrabold bg-yellow-100 border-yellow-200 px-3 py-5 rounded-xl border-4">
               You are't applied to any job yet.
@@ -24,14 +51,17 @@ const AppliedJobs = () => {
         <Banner />
         <div className="appliedJobContainer">
           <div className="flex justify-end items-center mt-10">
-            <select className="p-4 rounded-lg bg-gray-200 text-xl font-extrabold">
-              <option value="">Filter By</option>
-              <option value="">Name</option>
-              <option value="">Slavery</option>
+            <select
+              className="p-4 rounded-lg bg-gray-200 text-xl font-extrabold"
+              onChange={(btn) => filterByWhat(btn.target.value)}
+            >
+              <option>Filter By</option>
+              <option value="remote">Remote</option>
+              <option value="on-site">On-site </option>
             </select>
           </div>
           <div className="flex flex-col gap-5 mt-5">
-            {unFilteredData.map((item) => (
+            {appliedJobData.map((item) => (
               <JobItem jobData={item} key={item.id} />
             ))}
           </div>
