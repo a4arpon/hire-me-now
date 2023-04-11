6257dbaeeab4484399934cb3ad7bfe82c9012f7d
Mr. Xhr <a4arpon@gmail.com>
2023-04-12 01:34:40 +0600

    Styling Fixes

--- src/pages/Home/Hero.jsx
+++ src/pages/Home/Hero.jsx
@@ -4,9 +4,9 @@ const Hero = () => {
   return (
     <div className="heroContainer">
       <div className="heroText mb-5 lg:w-1/2">
-        <h1 className="text-4xl lg:text-7xl font-extrabold mb-5">
-          One Step Closer To Your{" "}
-          <span className="text-violet-400">Dream Job</span>
+        <h1 className="text-4xl lg:text-7xl font-extrabold mb-5 leading-snug">
+          One Step Closer To Your
+          <span className="text-violet-400"> Dream Job</span>
         </h1>
         <p className="mb-5 text-gray-500 text-xl font-medium">
           Explore thousands of job opportunities with all the information you
