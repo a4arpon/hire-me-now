6f905270eb6ebee5a0f342ccf4e2c405148e2847
Mr. Xhr <a4arpon@gmail.com>
2023-04-11 06:24:40 +0600

    Applied Job Loading Completed

--- src/pages/AppliedJobs/AppliedJobs.jsx
+++ src/pages/AppliedJobs/AppliedJobs.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import Banner from "./BannerAppliedJobs";
+import JobItem from "./JobItem";
 
 const AppliedJobs = () => {
   const unFilteredData = useLoaderData();
@@ -8,7 +9,20 @@ const AppliedJobs = () => {
   return (
     <div>
       <Banner />
-      <div className="jobContainer"></div>
+      <div className="appliedJobContainer">
+        <div className="flex justify-end items-center mt-10">
+          <select className="p-4 rounded-lg bg-gray-200 text-xl font-extrabold">
+            <option value="">Filter By</option>
+            <option value="">Name</option>
+            <option value="">Slavery</option>
+          </select>
+        </div>
+        <div className="flex flex-col gap-5 mt-5">
+          {unFilteredData.map((item) => (
+            <JobItem jobData={item} key={item.id} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/pages/AppliedJobs/JobItem.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const JobItem = () => {
+  return (
+    <div className="jobItem">
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default JobItem;
